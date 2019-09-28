869e5ef8a591991c1cb36acbc959b838b595e868
Seo,Youngseok <ysseo@berkeley.edu>
2019-09-28 14:11:38 +0900

    djfalj

--- Card_game/Card.cpp
+++ Card_game/Card.cpp
@@ -4,6 +4,13 @@
 
 Card::Card()
 {
+	
+}
+
+Card::Card(Shape Card_sha, int card_n)
+{
+	card_shape = Card_sha;
+	card_num = card_n;
 }
 
 
--- Card_game/Card.h
+++ Card_game/Card.h
@@ -1,8 +1,21 @@
 #pragma once
 class Card
 {
+private:
+	int card_num;
+	Shape card_shape;
+	//GUI 적용시킬때 이미지 같은 것이 필요하다.
 public:
 	Card();
+	Card(Shape Card_sha, int card_n);
+	int getCardNum();
+	Shape getCardShape();
 	~Card();
 };
 
+enum Shape {
+	Spade,
+	Diamond,
+	Heart,
+	Clober
+};
